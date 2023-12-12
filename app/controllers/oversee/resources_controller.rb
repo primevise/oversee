763e47a9849667bbc31479a9aa6fad1eeee50fad
Elvinas <elvinasnet@gmail.com>
2023-12-12 15:01:42 +0200

    adding barebones resource index

--- app/controllers/oversee/resources_controller.rb
+++ app/controllers/oversee/resources_controller.rb
@@ -1,11 +1,18 @@
 module Oversee
   class ResourcesController < BaseController
     def index
-      
+      @klass = resource_class
+      @resources = resource_class.all
     end
 
     def show
       
+    end
+
+    private
+
+    def resource_class
+      params[:resource].constantize
     end
   end
 end
