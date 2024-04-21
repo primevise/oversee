87a080e51fc295aab47c958026bad11aeac64d26
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2024-04-21 09:54:47 +0300

    wip

--- app/controllers/oversee/resources_controller.rb
+++ app/controllers/oversee/resources_controller.rb
@@ -23,7 +23,9 @@ module Oversee
       respond_to do |format|
         if @resource.update(resource_params)
           format.html { redirect_to resource_path(@resource.id, resource: @resource_class) }
-          format.turbo_stream
+          format.turbo_stream: [
+            turbo_stream.replace(dom_id(@resource, @key)) { render Oversee::Fields::DisplayRowComponent.new(key: @key, resource: @resource, datatype: @datatype, value: @resource.send(@key)) }
+          ]
         else
         end
       end
