dc10c78094aed4aa9d27aed355146473313eed24
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2024-06-05 11:58:43 +0300

    init metric

--- app/controllers/oversee/dashboard_controller.rb
+++ app/controllers/oversee/dashboard_controller.rb
@@ -1,7 +1,16 @@
 module Oversee
   class DashboardController < BaseController
     def show
-      
+      fetch_metrics
+    end
+
+    private
+
+    def fetch_metrics
+      @metrics = [{
+        label: "Total Users",
+        metric: User.count,
+      }]
     end
   end
 end
